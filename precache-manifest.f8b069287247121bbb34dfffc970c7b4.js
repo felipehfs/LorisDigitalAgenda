abef8828b109c757d028a4cfe821193309ed6c22
Felipe Henrique Ferreira sousa <felipehfsouza@gmail.com>
2019-04-07 17:31:40 -0300

    Updates

---
+++ precache-manifest.f8b069287247121bbb34dfffc970c7b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "245c469fb6555a60b6ef",
+    "url": "/LorisDigitalAgenda/static/js/runtime~main.3a939d1d.js"
+  },
+  {
+    "revision": "6793145e7dfdfff68203",
+    "url": "/LorisDigitalAgenda/static/js/main.2bf9f3dc.chunk.js"
+  },
+  {
+    "revision": "3e1b4bb61a39698f847d",
+    "url": "/LorisDigitalAgenda/static/js/2.3c91ee4b.chunk.js"
+  },
+  {
+    "revision": "6793145e7dfdfff68203",
+    "url": "/LorisDigitalAgenda/static/css/main.e402da50.chunk.css"
+  },
+  {
+    "revision": "aa19a72d7089e9544181e4cbc50fef8d",
+    "url": "/LorisDigitalAgenda/index.html"
+  }
+];
